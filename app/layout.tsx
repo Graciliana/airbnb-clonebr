ea0b30a320261c1eb4341ae7441c1ebf1e4a0696
user.email like : "eaGraciliana@Gmail.com <user.email like : "eaGraciliana@Gmail.com>
2023-05-09 20:27:41 -0300

    :tada: init: Inicial commit

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import './globals.css'
+import { Nunito } from 'next/font/google'
+
+export const metadata = {
+  title: 'Aribnb-BR',
+  description: 'Airbnb Clone',
+}
+const font = Nunito({
+  subsets: ['latin'],
+})
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  )
+}
